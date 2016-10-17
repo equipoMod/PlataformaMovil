de14ea2c2c427603800a45dd62180ab34543ba58
abcluis <abc_luis30@hotmail.com>
2016-10-17 17:38:29 -0600

    lueg

--- app/src/main/java/com/itchihuahuaii/plataformamovil/Main3Activity.java
+++ app/src/main/java/com/itchihuahuaii/plataformamovil/Main3Activity.java
@@ -1,13 +1,44 @@
 package com.itchihuahuaii.plataformamovil;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Main3Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        btn =(Button)findViewById(R.id.btn);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com/index.html"));
+        PendingIntent pendingIntent= PendingIntent.getActivity(this,0,intent,0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+        builder.setContentTitle("Notificacion Basica");
+        builder.setContentText("Tienes una tarea para subir hoy");
+        builder.setSubText("Toca para ver mas informacion");
+
+        NotificationManager notificationManager =(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(1,builder.build());
+
+
     }
 }
