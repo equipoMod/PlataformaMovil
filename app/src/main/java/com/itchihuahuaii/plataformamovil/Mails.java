11ef1122635cc94fc124a81ec1f1544ea1803fb6
abcluis <abc_luis30@hotmail.com>
2016-10-22 12:10:38 -0600

    Activity2 Launcher, listView custom, mod XML activity2

---
+++ app/src/main/java/com/itchihuahuaii/plataformamovil/Mails.java
@@ -0,0 +1,21 @@
+package com.itchihuahuaii.plataformamovil;
+
+/**
+ * Created by usuario1 on 10/22/2016.
+ */
+
+public class Mails {
+    public int icon;
+    public String title;
+
+    public Mails(){
+        super();
+    }
+
+    public Mails(int icon,String title){
+        super();
+        this.icon =icon;
+        this.title=title;
+    }
+
+}
--- app/src/main/java/com/itchihuahuaii/plataformamovil/Main2Activity.java
+++ app/src/main/java/com/itchihuahuaii/plataformamovil/Main2Activity.java
@@ -2,12 +2,31 @@ package com.itchihuahuaii.plataformamovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        listView = (ListView)findViewById(R.id.mail_list);
+
+        Mails[] mails = new Mails[]{
+                new Mails(R.drawable.homework_icon,"Tarea Moviles"),
+                new Mails(R.drawable.homework_icon,"Tarea Administracion"),
+                new Mails(R.drawable.homework_icon,"Presentacion Software"),
+                new Mails(R.drawable.homework_icon,"Tarea Gestion"),
+
+        };
+
+        MailAdapter adapter = new MailAdapter(this,R.layout.listview_item_row,mails);
+
+        View header = (View) getLayoutInflater().inflate(R.layout.list_header_row,null);
+        listView.addHeaderView(header);
+        listView.setAdapter(adapter);
     }
 }
