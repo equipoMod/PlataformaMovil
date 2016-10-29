e46aaa853a872adb75a9d24b0eba3685a3060cb8
abcluis <abc_luis30@hotmail.com>
2016-10-29 08:52:32 -0600

    Modifique casi todo y elimine cosas que no sirvieron de la parte que llevaba

--- app/src/main/java/com/itchihuahuaii/plataformamovil/MainActivity.java
+++ app/src/main/java/com/itchihuahuaii/plataformamovil/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_act3= (Button)findViewById(R.id.btn_act3);
